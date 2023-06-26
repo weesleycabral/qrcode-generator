6a8f509df6ea34be137d75ece37e069d530bea9d
weesleycabral <weesleylima@hotmail.com>
2023-06-26 16:54:08 -0300

    fazer download qr code dps

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as saveAs from 'file-saver';
-
+import html2canvas from 'html2canvas';
+// import * as html2canvas from 'html2canvas';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,44 +14,5 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  downloadQRCode(parent) {
-    // fetches base 64 date from image
-    const nav = window.navigator as any;
-    const parentElement = parent.el.nativeElement.querySelector('img').src;
-
-    // converts base 64 encoded image to blobData
-    let blobData = this.convertBase64ToBlob(parentElement);
-
-    // saves as image
-    if (window.navigator && nav.msSaveOrOpenBlob) {
-      //IE
-      nav.msSaveOrOpenBlob(blobData, 'Qrcode');
-    } else {
-      // chrome
-      const blob = new Blob([blobData], { type: 'image/png' });
-      const url = window.URL.createObjectURL(blob);
-      // window.open(url);
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = 'Qrcode';
-      link.click();
-    }
-  }
-
-  private convertBase64ToBlob(Base64Image: any) {
-    // SPLIT INTO TWO PARTS
-    const parts = Base64Image.split(';base64,');
-    // HOLD THE CONTENT TYPE
-    const imageType = parts[0].split(':')[1];
-    // DECODE BASE64 STRING
-    const decodedData = window.atob(parts[1]);
-    // CREATE UNIT8ARRAY OF SIZE SAME AS ROW DATA LENGTH
-    const uInt8Array = new Uint8Array(decodedData.length);
-    // INSERT ALL CHARACTER CODE INTO UINT8ARRAY
-    for (let i = 0; i < decodedData.length; ++i) {
-      uInt8Array[i] = decodedData.charCodeAt(i);
-    }
-    // RETURN BLOB IMAGE AFTER CONVERSION
-    return new Blob([uInt8Array], { type: imageType });
-  }
+  downloadQRCode() {}
 }
