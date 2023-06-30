7e484a7b933efcebf6b37aab93f7a326a2d60e15
weesleycabral <weesleylima@hotmail.com>
2023-06-30 16:36:32 -0300

    trocando package qrcode, agr download ta funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
-import { QrCodeModule } from 'ng-qrcode';
+// import { QrCodeModule } from 'ng-qrcode';
+import { QRCodeModule } from 'angularx-qrcode';
 import { HomeComponent } from './pages/home/home.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, QrCodeModule, FormsModule],
+  imports: [BrowserModule, QRCodeModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,20 +1,29 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public valor: any;
+  public valor = '';
+  public myAngularxQrCode: string = '';
+  public qrCodeDownloadLink: SafeUrl = '';
 
   constructor() {}
 
+  onChangeURL(url: SafeUrl) {
+    this.qrCodeDownloadLink = url;
+    console.log(this.myAngularxQrCode);
+  }
+
   ngOnInit(): void {}
 
-  downloadQRCode() {
-    const link = document.createElement('a');
-    link.href = this.valor;
-    link.download = 'qrcode.png';
-    link.click();
-  }
+  // downloadQRCode() {
+  //   console.log(this.valor.valueOf);
+  //   const link = document.createElement('a');
+  //   link.href = this.valor;
+  //   link.download = 'qrcode.png';
+  //   link.click();
+  // }
 }
